a65655447f36c4b32c2cb32cce39f5004d2358bc
unknown <tho.nguyenxuantho573@hcmut.edu.vn>
2023-11-28 18:13:49 +0700

    Create class to send response

---
+++ server/utils/response.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Utils;
+
+class Response
+{
+    // Properties
+
+    // Methods
+    public function __construct()
+    {
+    }
+
+    public function status($statusCode)
+    {
+        http_response_code($statusCode);
+        return $this;
+    }
+
+    public function header($header)
+    {
+        header($header);
+        return $this;
+    }
+
+    public function json($payload)
+    {
+        $this->header("Content-Type: application/json");
+        echo json_encode($payload);
+    }
+}
