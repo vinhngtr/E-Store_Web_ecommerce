bc1b0e4c61a9a143bafd320ea617c028ce8125a9
Vinh_Trong_Nguyen <vinhtrong782002@gmail.com>
2023-12-03 15:15:07 +0700

    add rating

---
+++ test/rating/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var stars = document.querySelectorAll(".star");
+  var ratingContainer = document.getElementById("ratingContainer");
+  var ratingStars = document.getElementById("ratingStars");
+  var selectedRating = document.getElementById("selectedRating");
+
+  stars.forEach(function (star) {
+    star.addEventListener("mouseover", function () {
+      highlightStars(star.dataset.value);
+    });
+
+    star.addEventListener("mouseout", function () {
+      clearHighlights();
+    });
+
+    star.addEventListener("click", function () {
+      var ratingValue = star.dataset.value;
+      selectedRating.textContent = "Selected rating: " + ratingValue;
+      ratingContainer.classList.add("rated");
+      clearHighlights();
+    });
+  });
+
+  function highlightStars(value) {
+    stars.forEach(function (star) {
+      if (star.dataset.value <= value) {
+        star.classList.add("selected");
+      }
+    });
+  }
+
+  function clearHighlights() {
+    stars.forEach(function (star) {
+      star.classList.remove("selected");
+    });
+  }
+});
