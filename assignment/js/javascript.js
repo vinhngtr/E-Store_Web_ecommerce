bf280a6179b4cf3fd0d3bd5f2e455e4f0df2ab2f
thanh thuan <thuanmt0712@hcmut.edu.vn>
2023-12-03 15:24:33 +0700

    ''

--- assignment/js/javascript.js
+++ assignment/js/javascript.js
@@ -1,23 +1,5 @@
 "use strict"
 
-// Tabs
-
-function openContent(evt, content) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("product__detail-tabs-content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(content).style.display = "flex";
-    evt.currentTarget.className += " active";
-}
-
-document.getElementById("defaultOpen").click();
-
 function openPayment(evt, PaymentMethod) {
   var i, PaymentContent, paymentlinks;
   PaymentContent = document.getElementsByClassName("PaymentContent");
