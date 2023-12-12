9b127c5546690dbe98b1f94f7c54042bd13c7a46
Nguyen Xuan Tho <xuantho573@gmail.com>
2023-12-12 18:55:05 +0700

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,13 +1,19 @@
 <?php
 
+// use Controller\Controller;
+// use Utils\Auth;
+
 include_once 'utils/env.php';
 include_once 'utils/request.php';
 include_once 'utils/response.php';
+include_once 'utils/auth.php';
 
 include_once 'Controller/controller.php';
 include_once 'Controller/product.controller.php';
 include_once 'Controller/order.controller.php';
 include_once 'Controller/cart.controller.php';
+include_once 'Controller/user.controller.php';
+include_once 'Controller/review.controller.php';
 
 // Load environment variables
 (new Utils\DotEnv(__DIR__ . '/.env'))->load();
@@ -16,34 +22,113 @@ include_once 'Controller/cart.controller.php';
 $request = new Utils\Request($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 $response = new Utils\Response();
 
+header('Access-Control-Allow-Origin: *');
+
+if (isset($_SERVER['HTTP_AUTHORIZATION']) && $_SERVER['HTTP_AUTHORIZATION'] !== '') {
+    if (!preg_match("/^Bearer \S+$/", $_SERVER['HTTP_AUTHORIZATION']))
+        $response->status(401)->json(array('message' => 'Invalid token'));
+    $_SERVER['HTTP_AUTHORIZATION'] = explode(' ', $_SERVER['HTTP_AUTHORIZATION'])[1];
+}
+// echo "Bearer token checked\n";
 $endpoint = $request->get_route(2);
 
-$tables = [
-    'brands' => 'brand',
-    // 'carts' => 'cart',
-    'categories' => 'category',
-    'orderDetails' => 'orderDetail',
-    'productColors' => 'productColor',
-    'productSizes' => 'productSize',
-    'reviews' => 'review',
-];
+// if (!in_array(($endpoint), ['', 'products'])) {
+//     Auth::validateToken($_SERVER['HTTP_AUTHORIZATION'] ?? '', $response);
+// }
 
 $controller = null;
 
-// Send request to corresponding controller
-if (array_key_exists($endpoint, $tables))
-    $controller = new Controller\Controller($tables[$endpoint]);
-// elseif ($endpoint === 'users')
-//     $controller = new Controller\UserController();
-elseif (in_array(($endpoint), ['', 'products']))
-    $controller = new Controller\ProductController();
-elseif ($endpoint === 'userOrders')
-    $controller = new Controller\OrderController();
-elseif ($endpoint === 'carts')
-    $controller = new Controller\CartController();
-else {
-    $response->status(401)->json(array('message' => 'Invalid request URI'));
-    exit();
+switch ($endpoint) {
+    case '':
+    case 'products':
+        // get
+        // get
+        // get post put delete
+        // checked
+        // DONE
+        $controller = new Controller\ProductController();
+        break;
+
+    case 'userOrders':
+        // 
+        // get post
+        // get put
+        // checked
+        // DONE
+        $controller = new Controller\OrderController();
+        break;
+
+    case 'carts':
+        // 
+        // get post put delete
+        // 
+        // checked
+        // DONE
+        $controller = new Controller\CartController();
+        break;
+
+    case 'brands':
+        // get
+        // get
+        // get post put delete
+        // checked
+        // DONE
+        $controller = new Controller\Controller(
+            'brand',
+            get: ['admin', 'user', ''],
+            post: ['admin'],
+            put: ['admin'],
+            delete: ['admin']
+        );
+        break;
+
+    case 'categories':
+        // get
+        // get
+        // get post put delete
+        // checked
+        // DONE
+        $controller = new Controller\Controller(
+            'category',
+            get: ['admin', 'user', ''],
+            post: ['admin'],
+            put: ['admin'],
+            delete: ['admin']
+        );
+        break;
+
+    case 'productColorSizes':
+        //
+        //
+        // post put delete
+        // checked
+        $controller = new Controller\Controller(
+            'productColorSize',
+            post: ['admin'],
+            put: ['admin'],
+            delete: ['admin']
+        );
+        break;
+
+    case 'reviews':
+        // get
+        // get post put
+        // get
+        // checked
+        // DONE
+        $controller = new Controller\ReviewController();
+        break;
+
+    case 'users':
+        // get
+        // get
+        // get post put delete
+        $controller = new Controller\UserController();
+        break;
+
+    default:
+        $response->status(400)->json(array('message' => 'Invalid request URI'));
+        break;
 }
 
 switch ($request->get_method()) {
@@ -60,5 +145,5 @@ switch ($request->get_method()) {
         $controller->delete($request, $response);
         break;
     default:
-        $response->status(405)->json(array('message' => 'Method not allowed'));
+        $response->status(405)->json(array('message' => 'Invalid request method'));
 }
